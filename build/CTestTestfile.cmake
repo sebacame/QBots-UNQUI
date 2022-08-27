d0c77a4ffbf962ccfc82707103cc998bdf1f11b0
sebacame <sebacame8@hotmail.com>
2022-08-27 02:58:29 -0300

    Release2

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cire2022/CIRE2022/catkin_ws/src
+# Build directory: /home/cire2022/CIRE2022/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bring_up")
+subdirs("exercises")
